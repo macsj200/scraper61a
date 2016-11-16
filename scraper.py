16feb7f0a0016ed0808c9cb7ba401512f6a291b4
suneelbelkhale <neelus.genius@gmail.com>
2016-11-15 17:50:46 -0800

    Working! Able to pull labs and hw, sorted and filtered.

--- scraper.py
+++ scraper.py
@@ -32,7 +32,7 @@ r = requests.get(urlBase)
 
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
-homework_links_raw = soup.find_all("a", string=re.compile("Homework"))
+homework_links_raw = soup.find_all("a", string=re.compile("Homework|Lab"))
 
 homework_links_filtered = list(set([homework_link["href"] for homework_link in homework_links_raw if re.compile("(lab/|hw/)").search(homework_link["href"])])) #lab/|
 #homework_links_filtered.remove('lab/lab00/')
@@ -43,24 +43,37 @@ url = "http://www-inst.eecs.berkeley.edu/~cs61a/fa15/hw/hw05/"
 r2 = requests.get(url)
 homework_page = bs4.BeautifulSoup(r2.text, 'html.parser')
 
+#gets the title of a page by finding the first h1 tag
+def getTitle(page):
+    titles = page.find_all("h1")
+    return titles[0].text if len(titles) != 0 else "~ No Title"
+    # for t in titles:
+        # print("TITLE: ", t.text)
+
+
+#iterates through all pages and outputs their values
 def iterate(homework_page):
     # for homework_page in homework_pages:
+    
     code_snippets = homework_page.find_all("pre")#homework_page.find("div", class_="col-md-9").child
     if code_snippets != None:
+        print(">>>>>>>> ON PAGE:", getTitle(homework_page), "<<<<<<<<")
 
         print("\n---- BUILDING CODE ----\n")
         for snippet in code_snippets:
-            print(snippet.text + "\n\n")
+            # print(snippet.text + "\n\n")
+            # execute the code in the pages to build it into the local scope
             if "python3" not in snippet.text: #and ("*** YOUR CODE HERE ***" in snippet.text or "_____" in snippet.text):
                 try:
+
                     exec(snippet.text.replace('_____', '5')) #completely random
-                except SyntaxError as e:
+                except Exception as e:
                     print("Did nothing for this code snippet --> " + str(e))
 
         scope = dict(locals())
         print("\n---- LOCAL DOC TESTS FOR PAGE ----\n")
         for var in scope:
-            # print(var); 
+            # print the doctests for all the compiled functions from pervious part (if they exist)
             if (isinstance(scope[var], types.FunctionType)): #and scope[var].__doc__ != "*** YOUR CODE HERE ***"):
                 if scope[var].__doc__ != None:
                     print(var + ": " + scope[var].__doc__ + "\n")
@@ -69,7 +82,15 @@ def iterate(homework_page):
 	#homework_questions = homework_page.find_all("h3", class_="question")
 	#print(homework_questions)
 
-# iterate(homework_page)
+#this part gets rid of duplicates
+homework_dict = {}
+for page in homework_pages:
+    homework_dict[getTitle(page)] = page
+homework_pages = homework_dict.values()
+#sort by title for convenience
+homework_pages = sorted(homework_pages, key=getTitle)
+
+#do the work
 for page in homework_pages:
     iterate(page)
 
